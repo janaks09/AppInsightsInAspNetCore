44aa75f7e1c89f71312b9c68e7b1335b3ebc0a62
Janak Shrestha <old_ink@hotmail.com>
2016-08-23 18:03:57 +0545

    Project added in Github

---
+++ src/AppInsightsSample/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace AppInsightsSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                TelemetryClient tc = new TelemetryClient();
+                tc.TrackPageView(new PageViewTelemetry("Index page") { Timestamp = DateTime.UtcNow }); //You can set up other options too.
+
+                throw new Exception("This is cutome exception");
+                //return View();
+            }
+            catch (Exception ex)
+            {
+                TelemetryClient tc = new TelemetryClient();
+                tc.TrackException(new ExceptionTelemetry(ex) { Timestamp = DateTime.UtcNow, SeverityLevel = SeverityLevel.Critical });
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
